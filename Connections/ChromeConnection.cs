8206d8cdd749bf70f4b24cea48e20710656d7df6
mimuogi <mimuogi@gmail.com>
2019-11-27 10:16:27 +0100

    using limpios

--- Connections/ChromeConnection.cs
+++ Connections/ChromeConnection.cs
@@ -2,11 +2,7 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace IEI_TelefonosBuscar.Connections
 {
